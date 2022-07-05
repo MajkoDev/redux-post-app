cc29d89af3ccbd145c34fd39c15cbdb6b58a242e
Marian <hellomariandev@gmail.com>
2022-07-05 21:43:45 +0200

    adding posts

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import {useDispatch} from "react-redux"
+import {nanoid} from "@reduxjs/toolkit"
+
+import {postAdded} from "./postsSlice"
+
+//? FORM TO ADD NEW CONTENT
+
+function AddPostForm() {
+
+    const dispatch = useDispatch()
+    
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+ 
+  const onTitleChange = (e) => setTitle(e.target.value);
+  const onContentChange = (e) => setContent(e.target.value);
+
+  // if we have title and content, then dispatch action
+  const onSavePostClick = () => {
+    if (title && content){
+        dispatch(
+            postAdded({
+                id: nanoid(), title, content
+            })
+        )
+        setTitle('')
+        setContent('')
+    }
+  }
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChange}
+        />
+
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChange}
+        />
+
+        <button type="button" onClick={onSavePostClick}>Save Post</button>
+      </form>
+    </section>
+  );
+}
+
+export default AddPostForm;
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -18,9 +18,16 @@ const initialState = [
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    // function for receiving new data
+    postAdded(state, action) {
+      state.push(action.payload);
+    },
+  },
 });
 
 export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
