d9dea91236bc0cca79bd48156c8185cddb39bfd1
Marian <hellomariandev@gmail.com>
2022-07-05 21:27:29 +0200

    set up posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,1 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content:
+      "The official, opinionated, batteries-included toolset for efficient Redux development.",
+  },
+  {
+    id: "2",
+    title: "React Query",
+    content:
+      'Fetch, cache and update data in your React and React Native applications all without touching any "global state".',
+  },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export default postsSlice.reducer;
